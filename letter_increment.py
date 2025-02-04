a109a7448ea9df0a3a22a1c028e6642720190a16
Sein <hosein.mufti@gmail.com>
2025-02-05 02:36:54 +0300

    Fixed incrementing last element

--- letter_increment.py
+++ letter_increment.py
@@ -1,25 +1,29 @@
-def main():
-    LETTER = 'A'
-    RANGE = 60
+LETTER = 'A'
+RANGE = 60
 
+
+def main():
     current_letter = LETTER
     letter_i = 0
     root = ''
+
     sequence = root + current_letter
+
     skip = 1
+
 
     for i in range(RANGE):
         current_letter, letter_i, bool_add_root = increment_letter(sequence[-1], letter_i)
 
+        if skip == 0:
+            root += '#'
+
+            skip = 1
+
         if bool_add_root:
             skip -= 1
-            
-            if skip < 1:
-                root += '#'
 
-                skip = 1
-
-        print(root)        
+        print(root + current_letter)
 
 
 def increment_letter(letter, i):
